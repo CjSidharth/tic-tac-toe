345025e8e279f02b5c23d5153be78bdab28358eb
CjSidharth <79306390+CjSidharth@users.noreply.github.com>
2024-12-14 23:48:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+
 
 class Game:
     # Creating a board upon calling
@@ -8,6 +10,16 @@ class Game:
         self.done = []
         self.no = 0
         self.win = False
+        self.ai = True
+
+
+    def mode(self):
+        try:
+            m = int(input("Enter 1 for AI and 2 for 2 players: "))
+            if(m == 2): self.ai = False
+        except ValueError or TypeError:
+            print("Enter either 1 or 2")
+
 
     # Declare player turn
     def declare(self):
@@ -21,7 +33,7 @@ class Game:
         print("Enter position")
         try:
             x = int(input())
-            if x in self.done:
+            if x in self.done or x < 1 or x > 9:
                 raise AssertionError
             else:
                 self.done.append(x)
@@ -33,10 +45,26 @@ class Game:
             self.no += 1
 
         except AssertionError:
-            print("You cannot overwrite another player's move!")
+            print("You cannot overwrite another player's move! / Don't make out of range moves")
     
         except TypeError:
             print("Enter integer position.")
+
+
+    def ai_turn(self):
+        try:
+            x = random.randint(1,9)
+            if x in self.done:
+                raise AssertionError
+            self.done.append(x)
+            i = (x//3) if x%3 != 0 else (x//3) - 1
+            j = (x%3) - 1
+            self.board[i,j] = self.turn
+            self.turn *= -1
+            self.no += 1
+        except AssertionError:
+            pass
+
 
     def display(self):
         for i in range(3):
@@ -70,10 +98,14 @@ class Game:
             return
              
     def game_loop(self):
+        self.mode()
         while(self.no < 9):
             self.display()
             self.declare()
-            self.player_turn()
+            if(self.ai == True and self.no%2 == 1):
+                self.ai_turn()
+            else:
+                self.player_turn()
             print(f"Turns {self.no}")
             self.win_check()
             if(self.win == True):
@@ -97,4 +129,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
